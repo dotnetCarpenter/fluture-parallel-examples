b8b04aae21b470d8938ca473d7f7bf1ddc6203e1
dotnetcarpenter <jon.ronnenberg@gmail.com>
2023-05-06 13:33:31 +0200

    implemented flatten :: ConcurrentFuture a (Future a b) -> Future a b

--- pap.js
+++ pap.js
@@ -1,68 +1,72 @@
 import sanctuary from "sanctuary";
 import {env as flutureEnv} from 'fluture-sanctuary-types';
-import {
-    after,
-    fork,
-    pap,
-    Par,
-    seq,
-    resolve,
-} from "fluture";
+import * as F from "fluture";
 
-const S = sanctuary.create ({checkTypes: false, env: sanctuary.env.concat (flutureEnv)});
+const S = sanctuary.create ({checkTypes: true, env: sanctuary.env.concat (flutureEnv)});
 
 const logWithTime = startTime => x => console.log (Date.now () - startTime, x);
 
-const getThingA = after (2000) ("A");
-const getThingB = after (2000) ("B");
-const getThingC = a => b => resolve (a + b + "C");
+// const getThingA = F.reject ("A");
+const getThingA = F.after (2000) ("A");
+const getThingB = F.after (2000) ("B");
+const getThingC = a => b => F.resolve (a + b + "C");
 
 let runPap1 = () =>
-    pap (getThingA) (pap (getThingB) (resolve (B => A => getThingC (A) (B)))).pipe (S.join);
+    F.pap (getThingA) (F.pap (getThingB) (F.resolve (B => A => getThingC (A) (B)))).pipe (S.join);
 
 runPap1 ()
-    .pipe (fork (console.error)
+    .pipe (F.fork (console.error)
                 (logWithTime (Date.now ())));
 
 
 
 const runPap2 = S.lift2 (getThingC)
-                        (Par (getThingA))
-                        (Par (getThingB));
+                        (F.Par (getThingA))
+                        (F.Par (getThingB));
 
-fork (console.error)
+F.fork (console.error)
      (logWithTime (Date.now ()))
-     (seq (runPap2).pipe (S.join));
+     (F.seq (runPap2).pipe (S.join));
 
 
 
 const runPap3 = S.lift2 (getThingC)
-                        (Par (getThingA));
+                        (F.Par (getThingA));
 
-const runProgram1 = S.pipe ([runPap3, seq, S.join]);
+const runProgram1 = S.pipe ([runPap3, F.seq, S.join]);
 
-fork (console.error)
+F.fork (console.error)
      (logWithTime (Date.now ()))
-     (runProgram1 (Par (getThingB)));
+     (runProgram1 (F.Par (getThingB)));
 
 
 
-const joinAfter = S.compose (S.join)
-const runProgram2 = S.compose (joinAfter (seq))
+const joinAfter   = S.compose (S.join);
+const runProgram2 = S.compose (joinAfter (F.seq))
                               (runPap3);
 
-fork (console.error)
+F.fork (console.error)
      (logWithTime (Date.now ()))
-     (runProgram2 (Par (getThingB)));
+     (runProgram2 (F.Par (getThingB)));
 
 
 
-fork (console.error)
+F.fork (console.error)
      (logWithTime (Date.now ()))
-     (seq (runPap2).pipe (S.join));
+     (joinAfter (F.seq) (runPap2));
 
 
 
-fork (console.error)
-     (logWithTime (Date.now ()))
-     (joinAfter (seq) (runPap2));
+
+// https://matrix.to/#/!tntZjsTSRQiWTjlnqS:gitter.im/$IIYMytDfilm5ewdu7KyCT6vo9VzvLNgsJqCuC05jEj0?via=gitter.im&via=matrix.org&via=aldwin.land
+// getThingA :: Future a b
+// getThingB :: Future a b
+// getThingC :: a -> b -> Future c d
+//    runPap :: ConcurrentFuture a (Future a b)
+
+//    flatten :: Applicative ConcurrentFuture, Monad Future => ConcurrentFuture a (Future a b) -> Future a b
+const flatten = S.compose (S.join)
+                          (F.seq);
+F.fork (console.error)
+       (logWithTime (Date.now ()))
+       (flatten (runPap2));
